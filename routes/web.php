0ce0cdd10f21a6b4d86e32f0d3ca01747151f1f3
shingo.matsuki <jinwusongmu20@gmail.com>
2020-07-18 12:37:41 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,6 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 
-
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
